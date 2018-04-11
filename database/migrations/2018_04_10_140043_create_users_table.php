24ee28c82b1e966194f0c0f5608569dca2d2a9ae
Timothy Teoh <timothy.teoh@frogasia.com>
2018-04-11 17:18:14 +0800

    fortrabbit

--- database/migrations/2018_04_10_140043_create_users_table.php
+++ database/migrations/2018_04_10_140043_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email', 100);
-            $table->string('password');
+            $table->string('password')->default('');
             $table->integer('gender')->nullable();
             $table->rememberToken();
             $table->timestamps();
