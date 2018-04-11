92b1b17656e92fed2cbf0fe79532b0ef945a09d2
Timothy Teoh <timothy.teoh@frogasia.com>
2018-04-11 16:19:04 +0800

    fr

--- database/migrations/2018_04_10_140043_create_users_table.php
+++ database/migrations/2018_04_10_140043_create_users_table.php
@@ -16,11 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('password');
             $table->integer('gender')->nullable();
             $table->rememberToken();
             $table->timestamps();
+
+            $table->index('email');
         });
     }
 
