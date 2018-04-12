dced48b0802efd9e82253fcb550c49281a4c08db
Timothy Teoh <timothy.teoh@frogasia.com>
2018-04-12 21:19:49 +0800

    build

--- public/undefined.js
+++ public/undefined.js
@@ -1,18 +1,1 @@
-webpackJsonp([3],{
-
-/***/ 302:
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__(303);
-
-
-/***/ }),
-
-/***/ 303:
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ })
-
-},[302]);
+webpackJsonp([3],{0:function(n,o,c){n.exports=c("1CH1")},"1CH1":function(n,o){}},[0]);
