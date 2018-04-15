ac941bee4023795294aa3ee972712b8da36f29f1
Timothy Teoh <timothy.teoh@frogasia.com>
2018-04-15 17:28:15 +0800

    fix translation typos

--- resources/assets/js/carpool.js
+++ resources/assets/js/carpool.js
@@ -35,7 +35,7 @@ ReactDOM.render(
                 <Col xsHidden>
                   {window.user && <Image style={{maxWidth: '40px'}} responsive src={window.user.avatar_url} /> }
                 </Col>
-                <a href="https://www.pulangmengundi.com">
+                <a href="/">
                   {/* <Glyphicon style={{border: '1px solid black', padding: '1px'}} glyph='remove' />&nbsp; */}
                   <strong>#PULANGMENGUNDI</strong>
                 </a>
