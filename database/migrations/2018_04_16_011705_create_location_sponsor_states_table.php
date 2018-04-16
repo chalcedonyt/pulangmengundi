df408a06ed960c73716820bd6760d9a040cf1006
Timothy Teoh <timothy.teoh@frogasia.com>
2018-04-16 09:19:52 +0800

    commit models

--- database/migrations/2018_04_16_011705_create_location_sponsor_states_table.php
+++ database/migrations/2018_04_16_011705_create_location_sponsor_states_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLocationSponsorRelationshipsTable extends Migration
+class CreateLocationSponsorStatesTable extends Migration
 {
     /**
      * Run the migrations.
