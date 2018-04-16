073de94e52b20aa3fc6ce850c8eb8369a1d38af7
Timothy Teoh <timothy.teoh@frogasia.com>
2018-04-16 09:20:22 +0800

    commit models

--- database/migrations/2018_04_16_011705_create_location_sponsor_states_table.php
+++ database/migrations/2018_04_16_011705_create_location_sponsor_states_table.php
@@ -16,8 +16,8 @@ class CreateLocationSponsorStatesTable extends Migration
         Schema::create('location_sponsor_states', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('location_sponsor_id');
-            $table->string('state_from', 200);
-            $table->string('state_to', 200);
+            $table->string('state_from', 150);
+            $table->string('state_to', 150);
             $table->index('location_sponsor_id');
             $table->index('state_from');
             $table->index('state_to');
