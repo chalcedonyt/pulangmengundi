ce6968da207c4f58bbed3b8d2f965eb0f17f7438
Timothy Teoh <timothy.teoh@frogasia.com>
2018-04-16 13:58:39 +0800

    Added analytics and redirect warning

---
+++ resources/assets/js/components/OutgoingSponsorModal.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import {Glyphicon, Button, Modal, Alert} from 'react-bootstrap'
+import {FormattedMessage, FormattedHTMLMessage} from 'react-intl'
+
+export default class OutgoingSponsorModal extends Component {
+  render() {
+    return (
+      <Modal show={this.props.show} onHide={this.props.onCancel}>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          <FormattedMessage
+            id="sponsor.redirect-title"
+            defaultMessage={`You are being redirected to our sponsor {outgoing}`}
+            values={{
+              outgoing: this.props.outgoingSponsor
+            }}
+          />
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Alert bsStyle='danger'>
+          <p>
+            <FormattedHTMLMessage
+              id="sponsor.redirect-warning-1"
+              defaultMessage={`If you have not created a listing on our site, <strong>please do so.</strong>`}
+            />
+          </p>
+          <p>
+            <FormattedHTMLMessage
+              id="sponsor.redirect-warning-2"
+              defaultMessage={`Sponsor tickets may have a verification delay and may have limited seats. To increase your chances, create a listing here before going to the sponsor site.`}
+            />
+          </p>
+        </Alert>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button bsStyle='success' target='_blank' onClick={this.props.onProceed} href={this.props.outgoingSponsorHref}>
+          <FormattedMessage
+            id="btn-proceed-sponsor"
+            defaultMessage={`Proceed to sponsor`}
+          />
+          <Glyphicon glyph='chevron-right' />
+        </Button>
+        <Button onClick={this.props.onCancel}>
+          <FormattedMessage
+            id="btn-cancel"
+            defaultMessage={`Cancel`}
+          />
+        </Button>
+      </Modal.Footer>
+    </Modal>
+    )
+  }
+}
