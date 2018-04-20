0272a8e77ce5c5b034dc366511fc6b65e4663228
Timothy Teoh <timothy.teoh@frogasia.com>
2018-04-21 00:47:50 +0800

    Added a job for apr 20 failed emails, and created a general page for 500 errors

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,11 @@
+@extends('shared.master')
+
+@section('title', 'Error')
+
+@section('content')
+<h1>An error occurred</h1>
+<div class="jumbotron">
+  <p>If you were trying to sign in with Facebook, please ensure that you have a valid email account registered with Facebook.</p>
+  <p>Please contact us at <a href='https://facebook.com/pulangmengundi/'>https://facebook.com/pulangmengundi/</a> or pulang.undi@gmail.com if your issue persists.</p>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('500', function () {
+    return view('errors.500');
+});
 Route::group([
     'prefix' => LaravelLocalization::setLocale(),
     'middleware' => [
